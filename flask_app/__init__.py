845bb52eaabafa190d951edb10c00790ba9a4fdd
aloha-fim <frederec.im@gmail.com>
2024-02-27 22:16:42 +0800

    models and users update

---
+++ flask_app/__init__.py
@@ -0,0 +1,78 @@
+# flask_app/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+from os import environ
+
+load_dotenv(".env")
+
+
+app = Flask(__name__)
+
+
+#########################
+###### DATABASE SETUP ###
+#########################
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = environ.get("SECRET_KEY")
+
+
+
+
+
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+app.app_context().push()
+
+############################################################
+# START localhost virtual environment ######################
+# virtualenv venv for python 2.7 and Windows
+# python3 -m venv venv for python 3+
+# source venv/bin/activate
+# .\venv\Scripts\activate for Windows
+# pip install -r requirements.txt for setup
+# pip freeze > requirements.txt for update after pip install
+# python3 app.py
+############################################################
+
+############################################################
+# Flask DB commands after pip3 install migrate workflow ####
+# 1) flask db init / flask db stamp head
+# 2) flask db migrate -m "first migration"
+# 3) flask db upgrade
+# to push migrations
+# 4) python3 app.py
+############################################################
+
+############################################################
+#stripe..x8>stripe login
+#stripe listen --forward-to localhost:4242/webhook
+#stripe trigger payment_intent.succeeded
+#stripe listen --forward-connect-to localhost:5000/listings/webhook
+#stripe trigger payment_intent.succeeded
+############################################################
+
+
+
+#################
+# LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+# register blueprints and
+
+from flask_app.core.views import core
+from flask_app.users.views import users
+from flask_app.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(users)
+app.register_blueprint(error_pages)
---
+++ flask_app/core/views.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, jsonify, Response
+from werkzeug.utils import secure_filename
+from pdf_processor import process_pdf_query
+
+core = Blueprint('core',__name__)
+
+@core.route('/flaskapp', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"error": "No file in the request"}), 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"error":"No selected file"}), 400
+
+        filename = secure_filename(file.filename)
+        file.save(filename)
+
+        question = request.form['question']
+        response = process_pdf_query(filename, question)
+
+        return render_template('upload.html', response=response)
+
+    return render_template('upload.html')
+
+@core.route('/test', methods=['GET','POST'])
+def test():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"error": "No file in the request"}), 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"error":"No selected file"}), 400
+
+        filename = secure_filename(file.filename)
+        file.save(filename)
+
+        question = request.form['question']
+        response = process_pdf_query(filename, question)
+
+        return render_template('index.html', response=response)
+
+    return render_template('test.html')
+
+
+@core.route('/bootstrap', methods=['GET','POST'])
+def bootstrap():
+
+    return render_template('index.html')
